9936c12951080381a20dd34a5f0d28013724f62c
Kevin Mok <kevin.mok@live.ca>
2017-10-21 15:10:41 -0400

    Found script to name colors.

--- js/index.js
+++ js/index.js
@@ -3,14 +3,17 @@ function rndColor() {
 }
 
 function applyNewColor(color) {
+	var match = ntc.name(color);
+
 	document.getElementsByTagName("body")[0].style.color = color;
 	document.getElementsByTagName("body")[0].style.backgroundColor = color;
 	document.getElementById("generate").style.backgroundColor = color;
-	$("#quote").html(color);
+	$("#quote").html(match[1]);
 }
 
 function applyRandomColor() {
 	color = rndColor();
+	// console.log(color);
 	applyNewColor(color);
 }
 
