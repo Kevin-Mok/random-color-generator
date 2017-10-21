9b9d48c3e6d2fb59e53e15348a201df7d0bd00ba
Kevin Mok <kevin.mok@live.ca>
2017-10-21 14:14:48 -0400

    Starts on random color.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
-$(document).ready(function() {
-	$("#generate").on("click", function() {
-		rndClr = randomColor({
-			luminosity: "dark"
-		});
-		document.getElementsByTagName("body")[0].style.color = rndClr;
-		document.getElementsByTagName("body")[0].style.backgroundColor = rndClr;
-		document.getElementById("generate").style.backgroundColor = rndClr;
-		$("#quote").html(rndClr);
+function newColor() {
+	rndClr = randomColor({
+		luminosity: "dark"
 	});
+	document.getElementsByTagName("body")[0].style.color = rndClr;
+	document.getElementsByTagName("body")[0].style.backgroundColor = rndClr;
+	document.getElementById("generate").style.backgroundColor = rndClr;
+	$("#quote").html(rndClr);
+}
+
+$(document).ready(function() {
+	newColor();
+	$("#generate").on("click",newColor);
 });
