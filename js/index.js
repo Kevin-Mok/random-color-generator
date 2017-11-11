aba29fe76f1a71b285f781256c0f29650ede5215
Kevin Mok <kevin.mok@live.ca>
2017-11-11 02:25:34 -0500

    Left/right history buttons are working.

--- js/index.js
+++ js/index.js
@@ -1,39 +1,61 @@
+var colorHistory = [];
+var curHistoryIndex = 0;
+
 function rndColor() {
-	return randomColor({ luminosity: "dark"	});
+    return randomColor({
+        luminosity: "dark"
+    });
 }
 
 function setDirButtonHeight() {
-	var height = document.getElementById("quote-box").offsetHeight;
-	//console.log(height + "px");
-	$('.dir-button-div').height(height);
+    var height = document.getElementById("quote-box").offsetHeight;
+    //console.log(height + "px");
+    $('.dir-button-div').height(height);
 }
 
-function applyNewColor(color) {
-	var match = ntc.name(color);
+function applyNewColor() {
+    var color = colorHistory[curHistoryIndex];
+    var match = ntc.name(color);
 
-	document.getElementsByTagName("body")[0].style.color = color;
-	document.getElementsByTagName("body")[0].style.backgroundColor = color;
-	document.getElementById("generate").style.backgroundColor = color;
-	$("#quote").html(match[1]);
-	setDirButtonHeight();
+    document.getElementsByTagName("body")[0].style.color = color;
+    document.getElementsByTagName("body")[0].style.backgroundColor = color;
+    document.getElementById("generate").style.backgroundColor = color;
+    $("#color-name").html(match[1]);
+    setDirButtonHeight();
 }
 
 function applyRandomColor() {
-	color = rndColor();
-	// console.log(color);
-	applyNewColor(color);
+    color = rndColor();
+    // console.log(color);
+    colorHistory.push(color);
+    curHistoryIndex = colorHistory.length - 1;
+    applyNewColor();
 }
 
-$(document).ready(function() {
-	applyRandomColor();
-	$("#generate").on("click", applyRandomColor);
-	/*
-	$(".dir-button-div")
-	.on("mouseenter", function() {
-	  $(".dir-button").show();
-	})
-	.on("mouseleave", function() {
-	  $(".dir-button").hide();
-	});
-	*/
+function applyPrevColor() {
+    if (curHistoryIndex > 0) {
+        curHistoryIndex--;
+    }
+    applyNewColor();
+}
+
+function applyNextColor() {
+    if (curHistoryIndex < colorHistory.length - 1) {
+        curHistoryIndex++;
+    }
+    applyNewColor();
+}
+
+$(document).ready(function () {
+    applyRandomColor();
+    $("#generate").on("click", applyRandomColor);
+    $(".dir-button-div")
+        .on("mouseenter", function () {
+            $(".dir-button").show();
+        })
+        .on("mouseleave", function () {
+            $(".dir-button").hide();
+        });
+    $("#dir-button-left").on("click", applyPrevColor);
+    $("#dir-button-right").on("click", applyNextColor);
 });
